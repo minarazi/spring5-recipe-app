5218204a19c89fc1a8c5bb350898665458208202
minarazi <razimina76@gmail.com>
2018-06-06 17:41:29 +0430

    add Enumerated to domain

---
+++ src/main/java/com/mina/domain/Difficulty.java
@@ -0,0 +1,5 @@
+package com.mina.domain;
+
+public enum Difficulty {
+
+}
--- src/main/java/com/mina/domain/Recipe.java
+++ src/main/java/com/mina/domain/Recipe.java
@@ -1,11 +1,14 @@
 package com.mina.domain;
 
+import java.util.Set;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Lob;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 @Entity
@@ -24,8 +27,12 @@ public class Recipe {
 	private String url;
 	private String directions;
 	
+	@OneToMany(cascade = CascadeType.ALL , mappedBy = "recipe" )
+	private Set<Ingredient> ingredient;
+	
 	@Lob
 	private Byte[] image;
+	private Difficulty difficulty;
 	@OneToOne(cascade = CascadeType.ALL)
 	private Notes notes;
 	public String getDescription() {
